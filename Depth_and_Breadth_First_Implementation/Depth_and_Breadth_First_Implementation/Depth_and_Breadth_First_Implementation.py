b9fd92e38d36a05a9846dc9fafe7814611f37017
Jason Riha <superjar30@gmail.com>
2020-09-16 21:47:02 -0500

    improvements

--- Depth_and_Breadth_First_Implementation/Depth_and_Breadth_First_Implementation/Depth_and_Breadth_First_Implementation.py
+++ Depth_and_Breadth_First_Implementation/Depth_and_Breadth_First_Implementation/Depth_and_Breadth_First_Implementation.py
@@ -1,67 +1,19 @@
 from collections import defaultdict
 
-class Graph():
-    def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        """
-        self.edges = defaultdict(list)
-    
-    def add_edge(self, from_node, to_node):
-        # Note: assumes edges are bi-directional
-        self.edges[from_node].append(to_node)
-        self.edges[to_node].append(from_node)
-
-graph = Graph()
-
-
-
-nodeList = [('A', 'B'),('A', 'C'),('B', 'D'),('B', 'E'),('C', 'F'),('C', 'H'),('D', 'I'),('D', 'J'),('E', 'G1'),('E', 'K'),('F', 'L'),('F', 'M'),('H', 'N'),('H', 'G2')]
 #nodeDictionary = {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F', 'H'], 'D': ['B', 'I', 'J'], 'E': ['B', 'G1', 'K'], 'F': ['C', 'L', 'M'], 'H': ['C', 'N', 'G2'], 'I': ['D'], 'J': ['D'], 'G1': ['E'], 'K': ['E'], 'L': ['F'], 'M': ['F'], 'N': ['H'], 'G2': ['H']}   This is nodeDictionary with being able to go back up the tree
-nodeDictionary = {'A': ['B', 'C'], 'B': ['D', 'E'], 'C': ['F', 'H'], 'D': ['I', 'J'], 'E': ['G1', 'K'], 'F': ['L', 'M'], 'H': ['N', 'G2'], 'I': [], 'J': [], 'G1': [], 'K': [], 'L': [], 'M': [], 'N': [], 'G2': []}
+nodeMap = {'A': ['B', 'C'], 'B': ['D', 'E'], 'C': ['F', 'H'], 'D': ['I', 'J'], 'E': ['G1', 'K'], 'F': ['L', 'M'], 'H': ['N', 'G2'], 'I': [], 'J': [], 'G1': [], 'K': [], 'L': [], 'M': [], 'N': [], 'G2': []}
 
-
-for node in nodeList:
-    graph.add_edge(*node)
-
-
-def breadthFirst(nodeDictionary):
+def depthFirst(nodeDictionary):
     openList = []
     closedList = []
     openList.append('A')
-
     while(len(openList) != 0):
         print("Open List: ", openList)
         print("Closed List: ", closedList)
         x = openList[0]
         openList.remove(openList[0])
         if(x == 'F'):
-            return 'Success'
-        else:
-            y = nodeDictionary[x]
-            closedList.insert(0, x)
-            for thing in y:
-                for node in closedList:
-                    if(thing == node):
-                        break
-                    else:
-                        openList.append(thing)
-                        break
-    return "Goal Not Found"
-
-
-def depthFirst(nodeDictionary):
-    openList = []
-    closedList = []
-    openList.append('A')
-
-    while(len(openList) != 0):
-        print("Open List: ", openList)
-        print("Closed List: ", closedList)
-        x = openList[0]
-        openList.remove(openList[0])
-        if(x == 'G1'):
+            print("You will need to travel through " + str(len(closedList) + 1) + " in order to get to node F.")
             return 'Success'
         else:
             y = nodeDictionary[x]
@@ -74,11 +26,38 @@ def depthFirst(nodeDictionary):
                         openList.insert(0, y[i])
                         break
     return "Goal Not Found"
-                        
 
+def breadthFirst(nodeDictionary):
+    openList = []
+    closedList = []
+    openList.append('A')
+    while(len(openList) != 0):
+        print("Open List: ", openList)
+        print("Closed List: ", closedList)
+        x = openList[0]
+        openList.remove(openList[0])
+        if(x == 'F'):
+            print("You will need to travel through " + str(len(closedList) + 1) + " in order to get to node F.")
+            return 'Success'
+        else:
+            y = nodeDictionary[x]
+            closedList.insert(0, x)
+            for thing in y:
+                for node in closedList:
+                    if(thing == node):
+                        break
+                    else:
+                        openList.append(thing)
+                        break
+    return "Goal Not Found"
+       
+def main():
+    #nodeDictionary = {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F', 'H'], 'D': ['B', 'I', 'J'], 'E': ['B', 'G1', 'K'], 'F': ['C', 'L', 'M'], 'H': ['C', 'N', 'G2'], 'I': ['D'], 'J': ['D'], 'G1': ['E'], 'K': ['E'], 'L': ['F'], 'M': ['F'], 'N': ['H'], 'G2': ['H']}   This is nodeDictionary with being able to go back up the tree
+    nodeDictionary = {'A': ['B', 'C'], 'B': ['D', 'E'], 'C': ['F', 'H'], 'D': ['I', 'J'], 'E': ['G1', 'K'], 'F': ['L', 'M'], 'H': ['N', 'G2'], 'I': [], 'J': [], 'G1': [], 'K': [], 'L': [], 'M': [], 'N': [], 'G2': []}
+    depthFirstAnswer = depthFirst(nodeMap)
+    breadthFirstAnswer = breadthFirst(nodeMap)
+    print(depthFirstAnswer)
+    print(breadthFirstAnswer)
 
-
-
-
-answer = depthFirst(nodeDictionary)
-print(answer)
+if __name__ == "__main__":
+    main()
